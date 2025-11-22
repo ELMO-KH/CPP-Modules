414d3895de6d375a1abc7fb4963a13fc887071e5
elmostafa kh <elmostafa_kh@localhost.localdomain>
2025-11-22 11:39:52 +0100

    ex01 -> ex03

---
+++ cpp01/ex03/HumanB.cpp
@@ -0,0 +1,15 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name)
+{
+    this->name = name;
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+    this->weapon = &weapon;
+}
+void HumanB::attack()
+{
+    std::cout << name << "attacks with their" << weapon->getType() << std::endl;
+}
